c23a97246f558954cc65f352abbb3f24293ca4e9
naokihakamada <51106808+naokihakamada@users.noreply.github.com>
2019-11-06 18:03:22 +0900

    404 design

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,18 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+<!------------------------->            
+<div class="row justify-content-center">
+        <div class="col-md-12">
+            <div class="ss-card card shadow alert alert-danger">
+                <div class="card-body float-none text-center">
+                    <h3 class="" style="margin-bottom:1em;margin-top:1em;">ファイルが存在いたしません</h3>
+                    <div><a href="/">ホームへ戻る</a><div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!------------------------->            
+</div>    
+@endsection
